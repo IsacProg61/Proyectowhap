26dbbc730bdaa61c32d8f8534aad0c66a4d263d5
Isaac Gonzalez <igonzalez@blacklabs.mx>
2026-03-08 01:13:36 -0600

    agrego otro origen, ya que no parece que se estaba conectando bien

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app = FastAPI(title="WhatsApp Clone API (Monolítico + Concurrencia Nativa)")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
